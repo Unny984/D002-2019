021d573c9ad66aba381bc47e9cb8f65d23f9272e
Unny984 <53845618+Unny984@users.noreply.github.com>
2019-08-07 12:30:04 +0800

    Add files via upload

---
+++ L1/Unny984(2).py
@@ -0,0 +1,4 @@
+from math import *
+r = input("What is the radius?\n")
+A = pi*r*2
+print(A)
---
+++ L1/Unny984(3.py
@@ -0,0 +1,5 @@
+score = float(input("What is your score in exam?\n"))
+if (score < 60):
+    print("You failed the exam")
+else:
+    print("You passed the exam")
